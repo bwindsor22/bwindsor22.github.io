57fd3699b80c6a5841b2d05174f04ba498ff2d52
Brad Windsor <bwindsor22@gmail.com>
2020-11-22 22:43:03 -0500

    Updates

--- precache-manifest.6ce4fe4b629fcde1451cb18ce2f7bf28.js
+++ precache-manifest.6ce4fe4b629fcde1451cb18ce2f7bf28.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8b0da785620190bb0ae2a4dd29499453",
+    "revision": "da669a200cabca586c4b5d487d1cea04",
     "url": "/index.html"
   },
   {
-    "revision": "17be4cf0041b810a7f24",
+    "revision": "54869dcb73aa1e159010",
     "url": "/static/css/2.33064c8f.chunk.css"
   },
   {
-    "revision": "c7d0faaff07bb7e43370",
+    "revision": "c294356dc10d43081fc5",
     "url": "/static/css/main.cfff658d.chunk.css"
   },
   {
-    "revision": "17be4cf0041b810a7f24",
-    "url": "/static/js/2.50379de2.chunk.js"
+    "revision": "54869dcb73aa1e159010",
+    "url": "/static/js/2.44d592d3.chunk.js"
   },
   {
     "revision": "b578178f33e8b4baed76c7b8f7ce2e07",
-    "url": "/static/js/2.50379de2.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.44d592d3.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c7d0faaff07bb7e43370",
-    "url": "/static/js/main.f075ad56.chunk.js"
+    "revision": "c294356dc10d43081fc5",
+    "url": "/static/js/main.4e36e051.chunk.js"
   },
   {
     "revision": "16d50208a04408d1bf24",
@@ -30,5 +30,9 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "d8b68062d368d2494b136f77a8264abb",
     "url": "/static/media/hike.d8b68062.jpg"
+  },
+  {
+    "revision": "b55e67d594769ab8f8d5bd290d256468",
+    "url": "/static/media/trees-game.b55e67d5.png"
   }
 ]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8e94425845f3937ec0ddcd7957f99898.js"
+  "/precache-manifest.6ce4fe4b629fcde1451cb18ce2f7bf28.js"
 );
 
 self.addEventListener('message', (event) => {
